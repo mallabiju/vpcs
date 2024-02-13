13be3f5171898b0625227bafbfef19165bf0f80c
Vijaya Malla <mallabiju@gmail.com>
2024-02-13 20:45:35 +0545

    First Commit

---
+++ A_Basic_IO/b_hello_nepal/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace b_hello_nepal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Nepal!");
+    }
+}
---
+++ A_Basic_IO/c_add_two_nums/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace c_add_two_nums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 78;
+        int b = 90;
+        Console.WriteLine("The sum = " + (a+b));
+    }
+}
