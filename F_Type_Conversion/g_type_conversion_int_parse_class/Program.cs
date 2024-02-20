1345b71910d6f60ae5b8f6c18f89beb307495d11
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-02-20 13:13:07 +0545

    Type Conversion using int.Parse() method

---
+++ F_Type_Conversion/g_type_conversion_int_parse_class/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace g_type_conversion_int_parse_class;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string stringValue = "123";
+        int intValue = int.Parse(stringValue);
+
+
+        System.Console.WriteLine("String Value: " + stringValue);
+        System.Console.WriteLine("Converted Integer Value: " + intValue);
+
+        int num = 100;
+        // Adding num(100) and Converted Integer Value(123)
+        int sum = num + intValue;
+        System.Console.WriteLine("The sum of " + num + " and " + intValue + " is = " + sum);
+
+    }
+}
