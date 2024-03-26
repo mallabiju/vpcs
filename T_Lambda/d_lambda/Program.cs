25b227a445581baa1a38d5141eb4aa02eabeb4df
Vijaya Malla <mallabiju@gmail.com>
2024-03-26 18:23:43 +0545

    Classwork solution

---
+++ T_Lambda/d_lambda/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace d_lambda;
+
+delegate int MyDel(int a, int b);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDel d = (a, b) => a > b ? a : b;
+        int result = d(10, 20);
+        Console.WriteLine("The larger value = " + result);
+    }
+}
